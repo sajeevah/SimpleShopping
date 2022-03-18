da64235bf5c1dc049fe06d8ff30376ad5ebbae23
sajeevah <sa.hasith@gmail.com>
2022-03-18 21:39:46 +0530

    add new table model

---
+++ SimpleShopping.Context/Models/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShopping.Context.Models
+{
+    public class Item
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public Guid CategoryId { get; set; }
+
+        [Required]
+        public Guid ItemModelId { get; set; }
+
+        [Required]
+        public Guid MakeId { get; set; }
+
+        [Required]
+        public string? SellerId { get; set; }
+
+        public string? ImageUrl { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual ICollection<ItemImage>? ItemImages { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category? Category { get; set; }
+        
+        [ForeignKey("ItemModelId")]
+        public virtual ItemModel? ItemModel { get; set; }
+
+        [ForeignKey("MakeId")]
+        public virtual Make? Make { get; set; }
+    }
+}
---
+++ SimpleShopping.Context/Models/ItemImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShopping.Context.Models
+{
+    public class ItemImage
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid ItemId { get; set; }
+
+        [Required]
+        public string? Url { get; set; }
+
+        [ForeignKey("ItemId")]
+        public virtual Item? Item { get; set; }
+
+    }
+}
