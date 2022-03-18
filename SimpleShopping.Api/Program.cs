9b70bb32e6e2b3aea4f6f84fa979b689a3b333de
sajeevah <sa.hasith@gmail.com>
2022-03-18 06:34:08 +0530

    add identity to project and run migration

--- SimpleShopping.Api/Program.cs
+++ SimpleShopping.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SimpleShopping.Context.Context;
+using SimpleShopping.Identity.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +10,15 @@ var configuration = new ConfigurationBuilder()
                 .Build();
 
 // Add services to the container.
+builder.Services.AddDbContext<SimpleShoppingContext>(x => x.UseSqlServer(configuration.GetConnectionString("SimpleShoppingConnectionString")));
+
+builder.Services.AddAuthServices(configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<SimpleShoppingContext>(x => x.UseSqlServer(configuration.GetConnectionString("SimpleShoppingConnectionString")));
 
 var app = builder.Build();
 
---
+++ SimpleShopping.Identity/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using SimpleShopping.Identity.Context;
+using System.Text;
+
+namespace SimpleShopping.Identity.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAuthServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<IndentityContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:ValidAudience"],
+                    ValidIssuer = configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                };
+            });
+
+            services.AddDbContext<IndentityContext>(cfg => {
+                cfg.UseSqlServer(configuration.GetConnectionString("SimpleShoppingIdentityConnectionString"));
+            });
+
+            return services;
+        }
+    }
+}
