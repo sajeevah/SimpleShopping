bcdb5586b70bcdbc4e1733f415a8cbcf7f70fe2b
sajeevah <sa.hasith@gmail.com>
2022-03-19 06:59:58 +0530

    category and itemModel controller

---
+++ SimpleShopping.Api/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleShopping.Context.Context;
+using SimpleShopping.Context.Models;
+
+namespace SimpleShopping.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly SimpleShoppingContext _context;
+
+        public CategoryController(SimpleShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Category
+        [HttpGet]
+        public IEnumerable<Category> GetCategores()
+        {
+            return _context.Category;
+        }
+
+        // GET api/Category/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(Guid id)
+        {
+            var category = await _context.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        // POST api/Category
+        [HttpPost]
+        public async Task PostCategory([FromBody] Category category)
+        {
+
+            _context.Category.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        // PUT api/Category/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(Guid id, [FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/Category/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return Ok(category);
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return _context.Category.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SimpleShopping.Api/Controllers/ItemModelController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleShopping.Context.Context;
+using SimpleShopping.Context.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleShopping.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemModelController : ControllerBase
+    {
+        private readonly SimpleShoppingContext _context;
+
+        public ItemModelController(SimpleShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ItemModel
+        [HttpGet]
+        public IEnumerable<ItemModel> GetCategores()
+        {
+            return _context.ItemModel;
+        }
+
+        // GET api/ItemModel/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemModel(Guid id)
+        {
+            var itemModel = await _context.ItemModel.FindAsync(id);
+
+            if (itemModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(itemModel);
+        }
+
+        // POST api/ItemModel
+        [HttpPost]
+        public async Task PostItemModel([FromBody] ItemModel itemModel)
+        {
+
+            _context.ItemModel.Add(itemModel);
+            await _context.SaveChangesAsync();
+        }
+
+        // PUT api/ItemModel/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItemModel(Guid id, [FromBody] ItemModel itemModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != itemModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(itemModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/ItemModel/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItemModel(Guid id)
+        {
+            var itemModel = await _context.ItemModel.FindAsync(id);
+            if (itemModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.ItemModel.Remove(itemModel);
+            await _context.SaveChangesAsync();
+
+            return Ok(itemModel);
+        }
+
+        private bool ItemModelExists(Guid id)
+        {
+            return _context.ItemModel.Any(e => e.Id == id);
+        }
+    }
+}
